0ddfccfceecb43fa14190532e4c2c59a3c0b194d
Michael Stonis <120685+michaelstonis@users.noreply.github.com>
2023-11-21 13:13:44 -0600

    upgrade to .NET 8

--- RxMAUI/GlobalUsings.cs
+++ RxMAUI/GlobalUsings.cs
@@ -2,5 +2,6 @@
 global using System.Reactive.Disposables;
 global using System.Reactive.Linq;
 global using CommunityToolkit.Maui.Markup;
+global using static CommunityToolkit.Maui.Markup.GridRowsColumns;
 global using ReactiveUI;
 global using ReactiveUI.Fody.Helpers;
